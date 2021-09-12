8621dd6c52ce3cad0f3a5f957f4c987713adf673
pya-py <p4ya.py@gmail.com>
2021-09-12 18:45:43 +0430

    t3d web server

--- controllers/webSocket.js
+++ controllers/webSocket.js
@@ -1,6 +1,7 @@
 const WebSocket = require("ws");
 
 const LAST_MOVE_KEY = "LAST_MOVE";
+const PLAYERS_KEY = "PLAYERS";
 
 var wss = undefined,
     rooms = [],
@@ -12,13 +13,13 @@ const createSocketCommand = (command, msg) =>
     });
 
 const leaveRoom = (roomName, playerID) => {
-    if (!rooms[roomName][playerID]) return; //this playerID doesnt exist  in the room; ignore it
+    if (!rooms[roomName][PLAYERS_KEY][playerID]) return; //this playerID doesnt exist  in the room; ignore it
     // if this player is the only one in the room
-    if (Object.keys(rooms[roomName]).length <= 1)
+    if (Object.keys(rooms[roomName][PLAYERS_KEY]).length <= 1)
         // delete the room entirely
         delete rooms[roomName];
     // if there are at least two in the room
-    else delete rooms[roomName][playerID]; // just remove this player
+    else delete rooms[roomName][PLAYERS_KEY][playerID]; // just remove this player
 };
 
 const forceSendLastMove = (roomName, client) => {
@@ -44,57 +45,69 @@ module.exports.setupWS = (server) => {
             if (request === "join") {
                 // console.log(roomName);
                 // if there is no room with this name, then create one
-                if (!rooms[roomName]) rooms[roomName] = [];
+                if (!rooms[roomName]) {rooms[roomName] = [];
+                    rooms[roomName][PLAYERS_KEY] = [];
+                }
                 //if the room exists(or already created),and the player is not, just add playerID in the room
                 //for each entry in each room, there is a key(playerID) and value(currect socket connection)
-                const turn = Object.keys(rooms[roomName]).length; //this turn is used for new players,
+                const turn = Object.keys(rooms[roomName][PLAYERS_KEY]).length; //this turn is used for new players,
                 // old players in the room, have thir previous turn
-                if (!rooms[roomName][playerID])
-                    rooms[roomName][playerID] = { socket, turn };
+                if (!rooms[roomName][PLAYERS_KEY][playerID])
+                    rooms[roomName][PLAYERS_KEY][playerID] = { socket, turn };
                 // ***** catch errors
                 //error 1: if the player is the third one in the room ===> sorry bro
 
                 socket.send(
                     createSocketCommand(
                         "SET_TURN",
-                        rooms[roomName][playerID].turn
+                        rooms[roomName][PLAYERS_KEY][playerID].turn
                     )
                 );
 
-                if (Object.keys(rooms[roomName]).length === 2) {
+                if (Object.keys(rooms[roomName][PLAYERS_KEY]).length === 2) {
                     //means two players are connected
                     //send message to each player to tell them the game is started
-                    Object.entries(rooms[roomName]).forEach(
+                    Object.entries(rooms[roomName][PLAYERS_KEY]).forEach(
                         ([, playerInTheRoom]) =>
                             playerInTheRoom.socket.send(
                                 createSocketCommand(
                                     "START",
-                                    Object.keys(rooms[roomName])
+                                    Object.keys(rooms[roomName][PLAYERS_KEY])
                                 )
                             )
                     );
                 }
                 // if turn  >1 ==> set the client as watcher
-                // console.log(rooms[roomName][playerID]);
+                // console.log(rooms[roomName][PLAYERS_KEY][playerID]);
             } else if (request === "move") {
-                console.table(Object.keys(rooms[roomName]));
-                console.log(roomName);
-                //********** */ u can use !turn to just send for opponent
-                Object.entries(rooms[roomName]).forEach(
-                    ([, playerInTheRoom]) => {
-                        if (socket !== playerInTheRoom.socket) {
-                            // send move to other client(player)
-                            // here is the summuary:
-                            // untill lastMove is not null => forceSend move
-                            // when move reciever, responds to MOVE ==> ,means move is recieved ==> stop forceSend
-                            // the last mad move, will be send to client to apply
-                            // if client recieves the move
-                            rooms[roomName][LAST_MOVE_KEY] = msg;
+                try {
+                    console.table(Object.keys(rooms[roomName][PLAYERS_KEY]));
+                    //********** */ u can use !turn to just send for opponent
+                    Object.entries(rooms[roomName][PLAYERS_KEY]).forEach(
+                        ([, playerInTheRoom]) => {
+                            try {
+                                if (socket !== playerInTheRoom.socket) {
+                                    // send move to other client(player)
+                                    // here is the summuary:
+                                    // untill lastMove is not null => forceSend move
+                                    // when move reciever, responds to MOVE ==> ,means move is recieved ==> stop forceSend
+                                    // the last mad move, will be send to client to apply
+                                    // if client recieves the move
+                                    rooms[roomName][LAST_MOVE_KEY] = msg;
 
-                            forceSendLastMove(roomName, playerInTheRoom.socket);
+                                    forceSendLastMove(
+                                        roomName,
+                                        playerInTheRoom.socket
+                                    );
+                                }
+                            } catch (err) {
+                                console.log(err);
+                            }
                         }
-                    }
-                );
+                    );
+                } catch (err) {
+                    console.log(err);
+                }
             } else if (request === "moveRecieved") {
                 // here: msg === recieved status
                 if (msg) {
