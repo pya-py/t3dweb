9b5dcf5b013f338ded756fee5d9770e3d538f1e9
pya-py <p4ya.py@gmail.com>
2021-09-14 19:15:35 +0430

    early demo for my tic-tac-toe 3d online project

--- controllers/webSocket.js
+++ controllers/webSocket.js
@@ -1,18 +1,152 @@
 const WebSocket = require("ws");
 
-const LAST_MOVE_KEY = "LAST_MOVE";
-const PLAYERS_KEY = "PLAYERS";
-
 var wss = undefined,
-    rooms = [],
-    lastMove = null;
+    rooms = [];
 const createSocketCommand = (command, msg) =>
     JSON.stringify({
         command,
         msg,
     });
 
-const leaveRoom = (roomName, playerID) => {
+const updateClientConnection = (roomName, client, newSocket) => {
+    client.socket = newSocket;
+    client.socket.send(
+        createSocketCommand("START", [
+            rooms[roomName].playerX.id,
+            rooms[roomName].playerO.id,
+        ])
+    );
+};
+
+const sendNewMoveTo = (roomName, client, newMove) => {
+    client.socket.send(createSocketCommand("MOVE", newMove));
+    rooms[roomName].lastMove = newMove;
+    rooms[roomName].emptyCells--;
+};
+
+module.exports.setupWS = (server) => {
+    wss = new WebSocket.Server({ server });
+    wss.on("connection", (socket) => {
+        socket.on("message", (data) => {
+            const { request, roomName, playerID, msg } = JSON.parse(data);
+            console.log(
+                "req:",
+                request,
+                "room:",
+                roomName,
+                "pid: ",
+                playerID,
+                "msg: ",
+                msg
+            );
+            if (!rooms[roomName].emptyCells) {
+                // determine the winner
+                // ...
+                //end game
+                const endCommand = createSocketCommand(
+                    "END",
+                    "winner yourTurn"
+                ); //replace msg param with winner's turn
+                rooms[roomName].playerX.socket.send(endCommand);
+                return;
+            }
+            // if game's not ended yet:
+            if (request === "join") {
+                // console.log(roomName);
+                // if there is no room with this name, then create one
+                if (!rooms[roomName]) {
+                    gameType = 4; //*****change this make client send the type of game */
+                    rooms[roomName] = {
+                        playerX: null,
+                        playerO: null,
+                        lastMove: null,
+                        emptyCells: gameType * gameType * gameType,
+                        table: [],
+                    };
+                }
+
+                if (!rooms[roomName].playerX) {
+                    rooms[roomName].playerX = { id: playerID, socket };
+                } else if (!rooms[roomName].playerO) {
+                    rooms[roomName].playerO = { id: playerID, socket };
+                } else {
+                    if (rooms[roomName].playerX.id === playerID) {
+                        updateClientConnection(
+                            roomName,
+                            rooms[roomName].playerX,
+                            socket
+                        );
+                    }
+                    //always get the latest socket connection ==> fixes connection lost problem
+                    else if (rooms[roomName].playerO.id === playerID) {
+                        updateClientConnection(
+                            roomName,
+                            rooms[roomName].playerO,
+                            socket
+                        );
+                    } else {
+                        // this a third client in the room!
+                        // u can set this client in a watcher array if you want to implement live watch
+                    }
+                    console.log("game started");
+
+                    //alternative for forceSendLastMove
+                    //resend the move to make sure moves are recieved on disconnect/connecting
+                    if (rooms[roomName].lastMove) {
+                        console.log(rooms[roomName]);
+                        socket.send(
+                            createSocketCommand(
+                                "MOVE",
+                                rooms[roomName].lastMove
+                            )
+                        );
+                    }
+                }
+
+                //always make sure yourTurn s are set in the both clients
+                rooms[roomName].playerX.socket.send(
+                    createSocketCommand("SET_TURN", 0)
+                );
+
+                rooms[roomName].playerO.socket.send(
+                    createSocketCommand("SET_TURN", 1)
+                );
+            } else if (request === "move") {
+                try {
+                    if (playerID === rooms[roomName].playerX.id) {
+                        console.log("sending move to player O ...");
+                        sendNewMoveTo(roomName, rooms[roomName].playerO, msg);
+                    } else if (playerID === rooms[roomName].playerO.id) {
+                        console.log("sending move to player X ...");
+                        sendNewMoveTo(roomName, rooms[roomName].playerX, msg);
+                    } else {
+                        //fuckin watcher maybe
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            } else if (request === "moveRecieved") {
+                // actually its not needed , but for caution only :)
+                // here: msg === recieved status
+                if (msg) {
+                    rooms[roomName].lastMove = null;
+                }
+            } else if (request === "leave") {
+                //leaveRoom(roomName);
+                console.log(`${playerID} left`); //comment this
+            }
+            // if (wss.clients.size <= 2) ws.send((wss.clients.size - 1).toString());
+        });
+
+        socket.on("close", (data) => {
+            // const { request, roomName, playerID, msg } = JSON.parse(data);
+            // leaveRoom(roomName);
+            // console.log(`${playerID} left`);
+        });
+    });
+};
+
+/*const leaveRoom = (roomName, playerID) => {
     if (!rooms[roomName][PLAYERS_KEY][playerID]) return; //this playerID doesnt exist  in the room; ignore it
     // if this player is the only one in the room
     if (Object.keys(rooms[roomName][PLAYERS_KEY]).length <= 1)
@@ -20,9 +154,8 @@ const leaveRoom = (roomName, playerID) => {
         delete rooms[roomName];
     // if there are at least two in the room
     else delete rooms[roomName][PLAYERS_KEY][playerID]; // just remove this player
-};
-
-const forceSendLastMove = (roomName, targetID) => {
+};*/
+/*const forceSendLastMove = (roomName, targetID) => {
     // mobile browser bug fixed
     // set a timeout numbers limit
     // or=> just resend move on re join ( it seems sufficent and send less commands )
@@ -43,124 +176,4 @@ const forceSendLastMove = (roomName, targetID) => {
     } catch (err) {
         console.log(err);
     }
-};
-module.exports.setupWS = (server) => {
-    wss = new WebSocket.Server({ server });
-    wss.on("connection", (socket) => {
-        socket.on("message", (data) => {
-            const { request, roomName, playerID, msg } = JSON.parse(data);
-            console.log("req:", request, "room:", roomName);
-            if (request === "join") {
-                // console.log(roomName);
-                // if there is no room with this name, then create one
-                if (!rooms[roomName]) {
-                    rooms[roomName] = [];
-                    rooms[roomName][PLAYERS_KEY] = [];
-                }
-                //if the room exists(or already created),and the player is not, just add playerID in the room
-                //for each entry in each room, there is a key(playerID) and value(currect socket connection)
-                const turn = Object.keys(rooms[roomName][PLAYERS_KEY]).length; //this turn is used for new players,
-                // old players in the room, have thir previous turn
-                if (!rooms[roomName][PLAYERS_KEY][playerID])
-                    rooms[roomName][PLAYERS_KEY][playerID] = { socket, turn };
-                else {
-                    rooms[roomName][PLAYERS_KEY][playerID].socket = socket;
-                }
-                // ***** catch errors
-
-                socket.send(
-                    createSocketCommand(
-                        "SET_TURN",
-                        rooms[roomName][PLAYERS_KEY][playerID].turn
-                    )
-                );
-                //error 1: if the player is the third one in the room ===> sorry bro
-                if (Object.keys(rooms[roomName][PLAYERS_KEY]).length === 2) {
-                    //means two players are connected
-                    //send message to each player to tell them the game is started
-                    console.log("game started");
-                    Object.entries(rooms[roomName][PLAYERS_KEY]).forEach(
-                        ([, playerInTheRoom]) =>
-                            playerInTheRoom.socket.send(
-                                createSocketCommand(
-                                    "START",
-                                    Object.keys(rooms[roomName][PLAYERS_KEY])
-                                )
-                            )
-                    );
-
-                    //alternative for forceSendLastMove
-                    //resend the move to make sure moves are recieved on disconnect/connecting
-                    if (rooms[roomName][LAST_MOVE_KEY]) {
-                        console.log(
-                            "last-move: ",
-                            rooms[roomName][LAST_MOVE_KEY]
-                        );
-                        console.table(rooms[roomName]);
-                        rooms[roomName][PLAYERS_KEY][playerID].socket.send(
-                            createSocketCommand(
-                                "MOVE",
-                                rooms[roomName][LAST_MOVE_KEY]
-                            )
-                        );
-                    }
-                }
-                // if turn  >1 ==> set the client as watcher
-                // console.log(rooms[roomName][PLAYERS_KEY][playerID]);
-            } else if (request === "move") {
-                try {
-                    //console.table(Object.keys(rooms[roomName][PLAYERS_KEY]));
-                    Object.entries(rooms[roomName][PLAYERS_KEY]).forEach(
-                        ([targetID, clientInTheRoom]) => {
-                            try {
-                                if (playerID !== targetID) {
-                                    console.log(
-                                        "send move to player: ",
-                                        clientInTheRoom.turn
-                                    );
-                                    // send move to other client(player)
-                                    // here is the summuary:
-                                    // untill lastMove is not null => forceSend move
-                                    // when move reciever, responds to MOVE ==> ,means move is recieved ==> stop forceSend
-                                    // the last mad move, will be send to client to apply
-                                    // if client recieves the move
-                                    rooms[roomName][LAST_MOVE_KEY] = msg;
-
-                                    //forceSendLastMove(roomName, targetID);
-
-                                    clientInTheRoom.socket.send(
-                                        createSocketCommand("MOVE", msg)
-                                    );
-                                }
-                            } catch (err) {
-                                console.log(err);
-                            }
-                        }
-                    );
-                } catch (err) {
-                    console.log(err);
-                }
-            } else if (request === "moveRecieved") {
-                // here: msg === recieved status
-                if (msg) {
-                    rooms[roomName][LAST_MOVE_KEY] = null;
-                }
-            } else if (request === "leave") {
-                //leaveRoom(roomName);
-                console.log(`${playerID} left`); //comment this
-            }
-            // if (wss.clients.size <= 2) ws.send((wss.clients.size - 1).toString());
-        });
-
-        // socket.on("moveRecieved", (data) => {
-        //     const {recieved, roomName} = JSON.parse(data);
-        //     if (recieved)rooms[roomName][LAST_MOVE_KEY] = null;
-        // });
-        socket.on("close", (data) => {
-            // what the fuck is wronge
-            // const { request, roomName, playerID, msg } = JSON.parse(data);
-            // leaveRoom(roomName);
-            // console.log(`${playerID} left`);
-        });
-    });
-};
+};*/
