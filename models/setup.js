520789f444171a01b6f66a73396f14f883c08c54
pya-py <p4ya.py@gmail.com>
2021-10-04 11:06:35 +0330

    T3D napping caused gameplay bug fixed =)))))

--- models/setup.js
+++ models/setup.js
@@ -5,6 +5,18 @@ const connectionString = "mongodb+srv://t3d:iust666cee@cluster0.iaxun.mongodb.ne
 const localConnectionString = "mongodb://localhost:27017/t3d";
 
 exports.connectToDB = () => {
+    /*mongoose.connection.collections['notices'].drop(function (err) {
+        console.log('notices dropped');
+    });
+    mongoose.connection.collections['games'].drop(function (err) {
+        console.log('games dropped');
+    });
+    mongoose.connection.collections['chats'].drop(function (err) {
+        console.log('games dropped');
+    });
+    mongoose.connection.collections['users'].drop(function (err) {
+        console.log('users dropped');
+    });*/
     return mongoose.connect(connectionString);
 };
 
